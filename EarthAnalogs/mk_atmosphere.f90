cd9babd99a2bfac597bb9449dc7e7d5cbd76bd00
Norbert Schorghofer <norbert@hawaii.edu>
2017-03-03 19:51:40 -1000

    updates

--- EarthAnalogs/mk_atmosphere.f90
+++ EarthAnalogs/mk_atmosphere.f90
@@ -1,7 +1,6 @@
-
 real(8) function mk_atmosphere(Z,I0,D0)
   ! returns direct and indirect irradiance on Mauna Kea in W/m^2
-  ! Nunez (1980), J. Biogeogr. 7, 173-186; corrected typos
+  ! in part based on Nunez (1980), J. Biogeogr. 7, 173-186; corrected typos
   implicit none
   real(8), intent(IN) :: Z  ! solar zenith angle (radians)
   real(8), intent(OUT) :: I0  ! clear-sky direct irradiance (unitless fraction) = transmittance
