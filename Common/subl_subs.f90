eeda61e317fb143c7f6f246072391c0daeccd2c2
nschorgh <norbert@psi.edu>
2019-04-17 09:03:48 -1000

    cleanup

--- Common/subl_subs.f90
+++ Common/subl_subs.f90
@@ -1,9 +1,9 @@
-! Functions that parametrizes sublimation rates into vacuum
+! Functions that parametrize sublimation rates into vacuum
 
 function sublrate(T)
   ! sublimation rate of H2O in #molecules/m^2/s
   implicit none
-  real(8),intent(IN) :: T
+  real(8), intent(IN) :: T
   real(8) sublrate
   real(8), parameter :: pi=3.141592653589793, kB = 1.38065e-23
   real(8), parameter :: mu = 18.015*1.66054e-27
@@ -12,21 +12,9 @@ function sublrate(T)
   ! crystalline ice
   sublrate = psv(T)/sqrt(2*pi*kB*T*mu);
 
-  ! amorphous ice according to Sack and Baragiola, 1993
+  ! amorphous ice according to Sack & Baragiola (1993)
   !sublrate = 1.82e21*1e4*T**3.5*exp(-0.45*1.6022e-19/(kB*T))
 end function sublrate
-
-
-pure function desorptionrate(T)
-  ! Hibbitts et al. (2011), Icarus
-  implicit none
-  real(8), intent(IN) :: T
-  real(8) desorptionrate
-  real(8), parameter :: nu = 1e13
-  real(8), parameter :: kB=1.38065e-23
-  real(8), parameter :: theta = 9.8947e+18
-  desorptionrate = nu*theta*exp(-0.5*1.6022e-19/(kB*T)) ! zeroth order
-end function desorptionrate
 
 
 pure function padsr(x)
@@ -52,14 +40,14 @@ end function padsr
 pure real(8) function restime_species(T)
   ! sublimation rate in #molecules/m^2/s
   implicit none
-  real*8,intent(IN) :: T
+  real*8, intent(IN) :: T
   real*8, parameter :: pi=3.1415926535897932, kB = 1.38065e-23
   real*8 psv, sublrate_species
   
   ! Argon
   real*8, parameter :: mu = 39.962*1.66054e-27
-  real*8, parameter :: A=-7814.5, B=+7.5741   ! Ict Vol 3
-  real*8, parameter :: sigma0 = 8.42e18   ! (1623/(40*1.66e-27))**(2./3.)  
+  real*8, parameter :: A=-7814.5, B=+7.5741   ! Int. Crit. Tbls. Vol 3
+  real*8, parameter :: sigma0 = 8.42e18   ! (1623/mu)**(2./3.)  
   psv = 10**(0.05223*A/T + B)*133.32  ! mmHg -> Pa
   
   ! CO2
